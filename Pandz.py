4176c7566d001cb2bf5d280dcbef0bef6f09e218
Mike98ssq <che.somehow@gmail.com>
2025-01-10 21:32:41 +0600

    Pandz.py

--- Pandz.py
+++ Pandz.py
@@ -5,7 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import seaborn as sns
-from sklearn.tree import *
 
 # Генерация случайных данных
 num_products = 10
